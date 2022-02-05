5021d4fcb9b04089daaacfca2c31f8a9703e9b9b
MBAYE SENE <smbaye@ept.sn>
2022-02-05 01:31:26 +0000

    création de la classe de base

--- EXO6/src/com/dic2/git/tpjava/exo6/Main.java
+++ EXO6/src/com/dic2/git/tpjava/exo6/Main.java
@@ -1,8 +1,11 @@
 package com.dic2.git.tpjava.exo6;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+        public static void main(String[] args) {
+
+                Random r = new Random();
+                System.out.println("The Randomly generated integer is : " + r.nextInt());
+            }
 }
