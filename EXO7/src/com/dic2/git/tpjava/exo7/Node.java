b076f3a5ce7631180459eb5fddf42b95195f92fb
MBAYE SENE <smbaye@ept.sn>
2022-02-05 23:23:44 +0000

    finalisation de l'exercice 7

---
+++ EXO7/src/com/dic2/git/tpjava/exo7/Node.java
@@ -0,0 +1,16 @@
+package com.dic2.git.tpjava.exo7;
+
+class Node {
+
+    char value;
+    Node left;
+    Node right;
+
+    Node(char value) {
+        this.value = value;
+        right = null;
+        left = null;
+    }
+
+
+}
